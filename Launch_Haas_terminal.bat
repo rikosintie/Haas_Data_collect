c6b97290a895471b5dc412cb7583da31fa4533c7
rikosintie <michael.hubbard999@gmail.com>
2025-12-14 21:00:18 -0800

    Git Title: Launch_Haas_terminal.bat
    Git Description:
    Batch file to launch scripts using Windows Terminal

---
+++ Launch_Haas_terminal.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Launch Haas loggers in separate tabs
+
+wt ^
+  new-tab -p "Haas Loggers ST40" ^
+  ; new-tab -p "Haas Loggers VF2SS" ^
+  ; new-tab -p "Haas Loggers VF5SS" ^
+  ; new-tab -p "Haas Loggers MINIMILL" ^
+  ; new-tab -p "Haas Loggers ST30" ^
+  ; new-tab -p "Haas Loggers ST30L"
