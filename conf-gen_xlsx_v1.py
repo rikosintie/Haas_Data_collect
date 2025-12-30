3dc29c4c17809b3aee7186189b1300c99b570df5
rikosintie <michael.hubbard999@gmail.com>
2025-12-29 21:03:04 -0800

    Git Title: conf-gen_xlsx_v1.py
    Git Description:
    Updated conf-gen_xlsx_v1.py to accept template
    argument and adjust output filenames accordingly
    
    This allows the script to generate the service files
    and systemd files based on the provided template.

--- conf-gen_xlsx_v1.py
+++ conf-gen_xlsx_v1.py
@@ -7,9 +7,11 @@ import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", help="xlsx name - ex. edge_info.xlsx")
+parser.add_argument("-f", "--file", help="xlsx name - ex. machines.xlsx")
+parser.add_argument("-t", "--template", help="template name - ex. service-template.txt")
 args = parser.parse_args()
 file = args.file
+template_name = args.template
 
 if file is None:
     print("-f file name is a required argument")
@@ -27,7 +29,8 @@ isExist = os.path.exists(path1)
 
 env = Environment(loader=FileSystemLoader(path1))
 
-template = env.get_template("service-template.txt")
+# template = env.get_template("service-template.txt")
+template = env.get_template(template_name)
 
 df = pd.read_excel(dev_inv_file)
 df.to_csv("temp.csv", index=False)
@@ -35,20 +38,10 @@ df.to_csv("temp.csv", index=False)
 with open("temp.csv") as f:
     routers = csv.DictReader(f)
     for router in routers:
-        # r1_conf = "./configs/" + router["name"] + ".service"
-        r1_conf = router["name"] + ".service"
+        if template_name == "service-template.txt":
+            # r1_conf = "./configs/" + router["name"] + ".service"
+            r1_conf = router["name"] + ".service"
+        elif template_name == "systemd-template.txt":
+            r1_conf = router["name"] + ".txt"
         with open(r1_conf, "w") as f:
             f.write(template.render(router))
-
-
-#    print(routers)
-#    print(routers.columns)
-#    print(routers['hostname'])
-#    list1 = routers['hostname']
-
-#    for router in routers:
-#        print(list1[i])
-#        r1_conf = './configs/' + list1[i] + '-1.txt'
-#        i += 1
-#        with open(r1_conf, 'w') as f:
-#            f.write(template.render(router))
