9e7fd1cafb5e9f299020b0190be270d25d55da0f
rikosintie <michael.hubbard999@gmail.com>
2026-02-11 17:30:08 -0800

    Git Title: Update ssh_port.sh to include firewall configuration file and user prompts
    Git Description:
    - Added functionality to update /etc/haas-firewall.conf with the new SSH port.
    - Improved user prompts and added clear instructions for the user after changing the SSH port.
    - Removed commented-out lines that were previously used for debugging or development purposes.
    
    removed
        read -r -p "Enter the SSH port number: " port
        # validate that the input is an integer
        if [[ "$port" =~ ^[0-9]+$ ]] && (( port >= 1024 )); then
    
    replaced with
    
        while true; do
        read -r -p "Enter the SSH port number (22, 1024-65535): " port
        # validate that the input is an integer and within the specified range
        if [[ "$port" =~ ^[0-9]+$ ]] && { [[ $port -eq 22 ]] || [[ $port -ge 1024 && $port -le 65535 ]]; }; then
        echo ""
            break
        else
            echo "Invalid port (must be 22, 1024-65535)"
            read -r -p "Try again? (y/n): " retry
            if [[ "$retry" != "y" && "$retry" != "yes" ]]; then
                echo "Exiting."
                exit 1
                fi
            fi
        done

--- ssh_port.sh
+++ ssh_port.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 echo ""
 echo ""
-echo "###########################################"
-echo "#                                         #"
-echo "#     Configure a custom port for SSH     #"
-echo "#                                         #"
-echo "###########################################"
+echo "############################################"
+echo "#                                          #"
+echo "#    Configure a custom port for SSH?      #"
+echo "#  Use a port between 22, or 1024-65535    #"
+echo "#                                          #"
+echo "############################################"
 echo ""
 echo ""
 # Ask user if they want to configure an SSH port
@@ -17,52 +18,47 @@ answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 
 if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
     #prompt for an integer
-    read -r -p "Enter the SSH port number: " port
-    # validate that the input is an integer
-    if [[ "$port" =~ ^[0-9]+$ ]] && (( port >= 1024 )); then
-    # if [[ "$port" =~ ^[0-9]+$ ]] && (( port >= 1024 )) && (( port =< 65535 )); then
-        SSH_PORT=\"$port\"
+    while true; do
+    read -r -p "Enter the SSH port number (22, 1024-65535): " port
+    # validate that the input is an integer and within the specified range
+    if [[ "$port" =~ ^[0-9]+$ ]] && { [[ $port -eq 22 ]] || [[ $port -ge 1024 && $port -le 65535 ]]; }; then
+    echo ""
+        break
+    else
+        echo "Invalid port (must be 22, 1024-65535)"
+        read -r -p "Try again? (y/n): " retry
+        if [[ "$retry" != "y" && "$retry" != "yes" ]]; then
+            echo "Exiting."
+            exit 1
+            fi
+        fi
+    done
         echo "SSH_PORT set to $port"
         # Update /etc/ssh/ssd_config
         echo "Updating /etc/ssh/sshd_config..."
         sudo sed -i "s/^#\?Port.*/Port $port/" /etc/ssh/sshd_config
+        # Update /etc/haas-firewall.conf
+        echo "Updating /etc/haas-firewall.conf..."
+        sudo sed -i "s/^SSH_PORT=.*/SSH_PORT=$port/" /etc/haas-firewall.conf
 
         # Reload systemd and restart SSH Service
+        echo ""
         echo "Restarting SSH Service..."
+        echo ""
         sudo systemctl daemon-reload
         sudo systemctl restart ssh.service
-        # echo "SSH service restarted with port $port"
+        echo ""
         sudo systemctl status ssh.service | grep "Server listening on 0.0.0.0 "
         sudo systemctl status ssh.service | grep "Server listening on :: "
-    else
         echo ""
-        echo "#######################################################"
-        echo "#                                                     #"
-        echo "#  Error: Please enter an integer between 1025-65535  #"
-        echo "#  You need to restart the script!                    #"
-        echo "#  Press the up arrow key and then [enter]            #"
-        echo "#                                                     #"
-        echo "#######################################################"
-        exit 1
-    fi
-else
-    SSH_PORT="22"
-echo ""
-echo ""
-echo "###########################################"
-echo "#                                         #"
-echo "#            SSH set to port 22           #"
-echo "#                                         #"
-echo "###########################################"
-echo ""
-echo ""
-    sudo sed -i "s/^#\?Port.*/Port $SSH_PORT/" /etc/ssh/sshd_config
-echo ""
-    echo "Restarting SSH Service..."
-    sudo systemctl daemon-reload
-    sudo systemctl restart ssh.service
-    sudo systemctl status ssh.service | grep "Server listening on 0.0.0.0 "
-    sudo systemctl status ssh.service | grep "Server listening on :: "
-echo ""
-echo ""
+        echo ""
+        echo "################################################"
+        echo "                                                "
+        echo "           Script is now complete!              "
+        echo "  The SSH service is configured for port $port     "
+        echo "        Use Cockpit to update the Firewall            "
+        echo "                                                "
+        echo "################################################"
+        echo ""
+        echo ""
 fi
