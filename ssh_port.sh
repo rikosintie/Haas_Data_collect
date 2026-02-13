e4529dfec5e8e9563454fe8694ad7241e4166138
rikosintie <michael.hubbard999@gmail.com>
2026-02-13 14:49:01 -0800

    Git Title: removed the y/n choice
    Git Description:
    removed the y/n choice since this is only run if you want to
    changes SSH to a custom port, so the question is redundant.
    Also updated the text to be more concise and clear.

--- ssh_port.sh
+++ ssh_port.sh
@@ -3,17 +3,17 @@ echo ""
 echo ""
 echo "#############################################"
 echo "#                                           #"
-echo "#     Configure a custom port for SSH?      #"
+echo "#      Configure a custom port for SSH      #"
 echo "#  Use port 22 or a port between 1024-65535 #"
 echo "#                                           #"
 echo "#############################################"
 echo ""
 echo ""
 # Ask user if they want to configure an SSH port
-read -r -p "Do you want use a custom ssh port? (y/n): " answer
+# read -r -p "Do you want use a custom ssh port? (y/n): " answer
 
 #  convert answer to lowercase for easier comparison
-
+answer="y"
 answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 
 if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
@@ -22,46 +22,46 @@ if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
     read -r -p "Enter the SSH port number (22, 1024-65535): " port
     # validate that the input is an integer and within the specified range
     if [[ "$port" =~ ^[0-9]+$ ]] && { [[ $port -eq 22 ]] || [[ $port -ge 1024 && $port -le 65535 ]]; }; then
-    echo ""
-        break
-    else
-        echo "Invalid port (must be 22, 1024-65535)"
-        read -r -p "Try again? (y/n): " retry
-        if [[ "$retry" != "y" && "$retry" != "yes" ]]; then
-            echo "Exiting."
-            exit 1
-            fi
+echo ""
+    break
+else
+    echo "Invalid port (must be 22, 1024-65535)"
+    read -r -p "Try again? (y/n): " retry
+    if [[ "$retry" != "y" && "$retry" != "yes" ]]; then
+        echo "Exiting."
+        exit 1
         fi
-    done
-        echo "SSH_PORT set to $port"
-        # Update /etc/ssh/ssd_config
-        echo "Updating /etc/ssh/sshd_config..."
-        sudo sed -i "s/^#\?Port.*/Port $port/" /etc/ssh/sshd_config
-        # Update /etc/haas-firewall.conf
-        echo "Updating /etc/haas-firewall.conf..."
-        sudo sed -i "s/^SSH_PORT=.*/SSH_PORT=$port/" /etc/haas-firewall.conf
+    fi
+done
+    echo "SSH_PORT set to $port"
+    # Update /etc/ssh/ssd_config
+    echo "Updating /etc/ssh/sshd_config..."
+    sudo sed -i "s/^#\?Port.*/Port $port/" /etc/ssh/sshd_config
+    # Update /etc/haas-firewall.conf
+    echo "Updating /etc/haas-firewall.conf..."
+    sudo sed -i "s/^SSH_PORT=.*/SSH_PORT=$port/" /etc/haas-firewall.conf
 
-        # Reload systemd and restart SSH Service
-        echo ""
-        echo "Restarting SSH Service..."
-        echo ""
-        sudo systemctl daemon-reload
-        sudo systemctl restart ssh.service
-        echo ""
-        sudo systemctl status ssh.service | grep "Server listening on 0.0.0.0 "
-        sudo systemctl status ssh.service | grep "Server listening on :: "
-        echo ""
-        echo ""
-        echo "##########################################################"
-        echo "                                                "
-        echo "           Script is now complete!              "
-        echo "  The SSH service is configured for port $port     "
-        echo "  /etc/haas-firewall.conf is updated with SSH_PORT=$port  "
-        echo "        Use Cockpit to update the Firewall            "
-        echo "                                                "
-        echo "##########################################################"
-        echo ""
-        echo ""
+    # Reload systemd and restart SSH Service
+    echo ""
+    echo "Restarting SSH Service..."
+    echo ""
+    sudo systemctl daemon-reload
+    sudo systemctl restart ssh.service
+    echo ""
+    sudo systemctl status ssh.service | grep "Server listening on 0.0.0.0 "
+    sudo systemctl status ssh.service | grep "Server listening on :: "
+    echo ""
+    echo ""
+    echo "##########################################################"
+    echo "                                                "
+    echo "           Script is now complete!              "
+    echo "  The SSH service is configured for port $port     "
+    echo "  /etc/haas-firewall.conf is updated with SSH_PORT=$port  "
+    echo "        Use Cockpit to update the Firewall            "
+    echo "                                                "
+    echo "##########################################################"
+    echo ""
+    echo ""
 else
     SSH_PORT="22"
     echo ""
