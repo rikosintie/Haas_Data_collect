ca8c1c7ccd3a6c78ae211e57f826139c53110fd0
rikosintie <michael.hubbard999@gmail.com>
2026-05-02 15:08:26 -0700

    Git Title: haas-aliases.zsh: - Custom aliases for managing Haas appliance and Samba shares
    Git Description:
    copy to ~/.oh-my-zsh/custom
    
    Add new aliases for managing Haas appliance and Samba shares

---
+++ haas-aliases.zsh
@@ -0,0 +1,40 @@
+# Example aliases
+alias _='sudo '
+alias treeh='tree -h'
+alias treed='treed -dh'
+alias cat='batcat'
+# export BAT_THEME="Coldark-Cold"
+export BAT_THEME="zenburn"
+
+# Display Linux users
+alias lusers='awk -F: '\''$3 >= 1000 {print $1}'\'' /etc/passwd'
+
+# Display Samba users
+alias susers='sudo pdbedit -L 2>/dev/null | cut -d: -f1'
+
+# Display hass services
+alias haasserv='systemctl list-unit-files --type=service | grep haas'
+
+# Tailspin Aliases
+alias t-cockpit='sudo journalctl -u cockpit -f | tspin'
+alias t-health='sudo journalctl -u smbd -u ssh -u cockpit -f | tspin'
+alias t-samba='sudo tail -f /var/log/samba/log.smbd | tspin'
+alias t-ssh='sudo tail -f /var/log/auth.log | tspin'
+# log for UFW with filtering for multicast traffic
+alias t-ufw='sudo grep -E 'UFW ' /var/log/syslog | grep -Ev 'DST=224\.' | tspin'
+# live log for UFW with filtering for multicast traffic
+alias t-ufwl='sudo tail -f /var/log/syslog | grep -E 'UFW ' | grep -Ev 'DST=224\.' | tspin'
+
+#Directory aliases
+alias haas-log='cd /var/log/'
+alias haas-fw-conf='sudo fresh /etc/haas-firewall.conf'
+alias haas-repo='cd /home/haas/Haas_Data_collect/'
+alias haas-bin='cd /usr/local/sbin'
+alias haas-firewall='cd /usr/share/cockpit/haas-firewall/'
+alias haas-samba='cd /usr/share/cockpit/manage-samba/'
+alias haas-updates='cd /usr/share/cockpit/update-appliance/'
+alias haas-systemd() {
+    'cd /etc/systemd/system/'
+    'l haas-*.service'
+}
+alias haas-sshd='cd /etc/ssh/sshd_config.d/'
