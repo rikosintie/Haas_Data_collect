e1d847998221287e1ab6677aea6828e607cb84f5
rikosintie <michael.hubbard999@gmail.com>
2026-05-01 18:54:54 -0700

    Git Title: cockpit_samba/list_shares.sh: - Fixed share duplicaton issue

--- cockpit_samba/list_shares.sh
+++ cockpit_samba/list_shares.sh
@@ -3,27 +3,30 @@
 
 testparm -s 2>/dev/null | awk '
 BEGIN {
-    FS="=";
     printf "%-20s %-40s %-25s %-10s\n", "SHARE", "PATH", "VALID USERS", "READ ONLY";
+    share=""; path=""; users=""; ro="";
 }
 /^\[/ {
-    share=$0;
-    gsub(/[\[\]]/, "", share);
-
-    if (share != "global") {
-        path=""; users=""; ro="";
+    # Print the previous share when the next [section] header is seen
+    if (share != "" && share != "global") {
+        printf "%-20s %-40s %-25s %-10s\n", share, path, users, ro;
     }
+    share = $0;
+    gsub(/[\[\]]/, "", share);
+    gsub(/^[ \t]+|[ \t]+$/, "", share);
+    path=""; users=""; ro="";
 }
 /^[ \t]*path[ \t]*=/ {
-    path=$2; gsub(/^[ \t]+/, "", path);
+    val = $0; sub(/^[ \t]*path[ \t]*=[ \t]*/, "", val); path = val;
 }
 /^[ \t]*valid users[ \t]*=/ {
-    users=$2; gsub(/^[ \t]+/, "", users);
+    val = $0; sub(/^[ \t]*valid users[ \t]*=[ \t]*/, "", val); users = val;
 }
 /^[ \t]*read only[ \t]*=/ {
-    ro=$2; gsub(/^[ \t]+/, "", ro);
+    val = $0; sub(/^[ \t]*read only[ \t]*=[ \t]*/, "", val); ro = val;
 }
-/^$/ {
+END {
+    # Print the last share
     if (share != "" && share != "global") {
         printf "%-20s %-40s %-25s %-10s\n", share, path, users, ro;
     }
